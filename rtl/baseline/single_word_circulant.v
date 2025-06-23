6312b0bc9fd8fd783b480f9a90a44432b01e31e8
Erica <erica@untether.ai>
2025-06-23 08:28:28 -0400

    Less functionally incorrect

--- rtl/baseline/single_word_circulant.v
+++ rtl/baseline/single_word_circulant.v
@@ -47,7 +47,7 @@ always @(posedge clk) begin
     if (write_en) begin
         // Loop over data_in's chunks, store each in the right column
         for (w_chunk = 0; w_chunk < COLS_PER_WORD; w_chunk = w_chunk + 1) begin
-            s_w_col <= circulant_col_addr(write_row, write_col, w_chunk[ADDR_LEN-1:0]);
+            s_w_col = circulant_col_addr(write_row, write_col, w_chunk[ADDR_LEN-1:0]);
             col_mem[write_row][s_w_col] <= data_in[w_chunk * COL_WIDTH +: COL_WIDTH];
         end
     end
@@ -56,14 +56,14 @@ end
 // Read logic
 integer r_chunk;
 reg [ADDR_LEN-1:0] s_r_col; // shifted read col
-reg [ADDR_LEN-1:0] r_row; // increment read row each chunk
+reg [ADDR_LEN-1:0] current_r_row; // increment read row each chunk
 always @(posedge clk) begin
     if (read_en) begin
-        r_row <= read_row; // reset row for each read
+        current_r_row = read_row; // reset row for each read
         for (r_chunk = 0; r_chunk < COLS_PER_WORD; r_chunk = r_chunk + 1) begin
-            s_r_col <= circulant_col_addr(read_row, read_col, r_chunk[ADDR_LEN-1:0]);
-            data_out[r_chunk * COL_WIDTH +: COL_WIDTH] <= col_mem[r_row][s_r_col];
-            r_row <= (r_row + 1'b1) & ADDR_LEN'(ADDR_MASK); // increment row for next chunk
+            s_r_col = circulant_col_addr(read_row, read_col, r_chunk[ADDR_LEN-1:0]);
+            data_out[r_chunk * COL_WIDTH +: COL_WIDTH] <= col_mem[current_r_row][s_r_col];
+            current_r_row = (current_r_row + 1'b1) & ADDR_LEN'(ADDR_MASK); // increment row for next chunk
         end
     end
 end
